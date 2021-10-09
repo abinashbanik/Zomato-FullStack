5d4c8d1ef3ab71e21b1198c492d497f528009eca
abinashbanik <abinashbanik2016@gmail.com>
2021-10-09 13:14:43 +0530

    fixed navbar

--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -6,7 +6,7 @@ import { RiSearch2Line } from "react-icons/ri";
 
 const MobileNav = () => {
     return (
-    <div className="flex items-center justify-between md:hidden">
+    <div className="flex items-center justify-between w-full lg:hidden">
         <div className="w-28">
             <img 
                 src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" 
@@ -22,69 +22,70 @@ const MobileNav = () => {
     );
 };
 
-const MediumNav = () => {
+const LargeNav = () => {
     return (
     <>
-        <div className="hidden md:flex items-center justify-between gap-4 lg:w-3/4 ">
-            <div className="w-28">
-                <img 
-                    src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" 
-                    alt="logo"
-                    className="w-full h-full" 
-                />
-            </div>
-            <div className="w-full flex items-center gap-3 bg-white shadow-md p-3 border-gray-200 rounded">
-                <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
-                    <span className="text-zomato-400"><HiLocationMarker /></span>
-                    <input type="text" className="" placeholder="Agartala" className="focus:outline-none" />
-                    <IoMdArrowDropdown />
+        <div className="container px-20 mx-auto">
+            <div className="hidden w-full  lg:flex items-center justify-around gap-4 ">
+                <div className="w-28">
+                    <img 
+                        src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" 
+                        alt="logo"
+                        className="w-full h-full" />
                 </div>
-                <div className="flex items-center gap-2 w-full">
-                    <RiSearch2Line />
-                    <input type="search" 
-                            className="w-full focus:outline-none"
-                            placeholder="Search fro restaurant, dish or cuisine" />
+                <div className="w-3/4 flex items-center gap-3 bg-white shadow-md p-3 border border-gray-200 rounded">
+                    <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+                        <span className="text-zomato-400"><HiLocationMarker /></span>
+                        <input type="text" className="" placeholder="Agartala" className="focus:outline-none" />
+                        <IoMdArrowDropdown />
+                    </div>
+                    <div className="flex items-center gap-2 w-full">
+                        <RiSearch2Line />
+                        <input type="search" 
+                                className="w-full focus:outline-none"
+                                placeholder="Search fro restaurant, dish or cuisine" />
+                    </div>
                 </div>
-            </div>
-            <div className="flex items-center gap-2 ">
-                <button className="text-gray-500 hover:text-gray-900 text-xl">Login</button>
-                <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
+                <div className="flex ml-28 gap-4 ">
+                    <button className="text-gray-500 hover:text-gray-900 text-xl">Login</button>
+                    <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
+                </div>
             </div>
         </div>
     </>
     );
 };
 
-const LargeNav = () => {
-    return (
-    <>
-        <div className="hidden lg:flex items-center justify-between gap-4">
-            <div className="w-28">
-                <img 
-                    src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" 
-                    alt="logo"
-                    className="w-full h-full" 
-                />
-            </div>
-            <div className="w-full flex items-center gap-3 bg-white shadow-md p-3 border-gray-200 rounded">
-                <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
-                    <span className="text-zomato-400"><HiLocationMarker /></span>
-                    <input type="text" className="" placeholder="Agartala" />
-                    <IoMdArrowDropdown />
-                </div>
-                <div className="flex items-center gap-2">
-                    <RiSearch2Line />
-                    <input type="search" placeholder="Search fro restaurant, dish or cuisine" />
-                </div>
-            </div>
-            <div className="flex items-center gap-2">
-                <button className="text-gray-500 hover:text-gray-900 text-xl">Login</button>
-                <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
-            </div>
-        </div>
-    </>
-    );
-};
+// const MediumNav = () => {
+//     return (
+//     <>
+//         <div className="hidden lg:flex items-center justify-between gap-4 w-full lg:w-3/4">
+//             <div className="w-28">
+//                 <img 
+//                     src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" 
+//                     alt="logo"
+//                     className="w-full h-full" 
+//                 />
+//             </div>
+//             <div className="w-full flex items-center gap-3 bg-white shadow-md p-3 border-gray-200 rounded">
+//                 <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+//                     <span className="text-zomato-400"><HiLocationMarker /></span>
+//                     <input type="text" className="" placeholder="Agartala" />
+//                     <IoMdArrowDropdown />
+//                 </div>
+//                 <div className="flex items-center gap-2">
+//                     <RiSearch2Line />
+//                     <input type="search" placeholder="Search fro restaurant, dish or cuisine" />
+//                 </div>
+//             </div>
+//             <div className="flex items-center gap-2">
+//                 <button className="text-gray-500 hover:text-gray-900 text-xl">Login</button>
+//                 <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
+//             </div>
+//         </div>
+//     </>
+//     );
+// };
 
 const Navbar = () => {
     return (
@@ -92,7 +93,7 @@ const Navbar = () => {
             <nav className="p-4  w-full bg-white shadow-md  ">
             
                 <MobileNav />
-                <MediumNav />    
+                <LargeNav />    
                   
             </nav>
         </>
